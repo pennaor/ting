5e98650d6a68be986506a8aa18818cea285cbca1
Aluisio Penna Ordones <aluisioordones1@gmail.com>
2023-04-04 16:37:16 -0300

    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    name_ext = path_file.split(".")
+    if len(name_ext) != 2 or name_ext[1] != "txt":
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file) as file:
+            content = file.read()
+        return content.split("\n")
+    except(FileNotFoundError):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
