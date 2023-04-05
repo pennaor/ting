d88e5daf6a6dfb6972c4597171e49c5f5e87052c
Aluisio Penna Ordones <aluisioordones1@gmail.com>
2023-04-05 12:01:45 -0300

    refactor(file_process)

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,29 +1,30 @@
+import sys
 from ting_file_management.file_management import txt_importer
-# from ting_file_management.queue import Queue
 
 
 def is_file_unique(path_file, instance):
     path_file_unique = True
     for index in range(len(instance)):
         try:
-            process_dict = instance.search(index)
-            if process_dict["nome_do_arquivo"] == path_file:
+            file_metadata = instance.search(index)
+            if file_metadata["nome_do_arquivo"] == path_file:
                 path_file_unique = False
-        except(IndexError):
+        except (IndexError):
             break
     return path_file_unique
 
 
 def process(path_file, instance):
-    if is_file_unique(path_file, instance):
-        rows = txt_importer(path_file)
-        process_dict = dict({
-            "nome_do_arquivo": path_file,
-            "qtd_linhas": len(rows),
-            "linhas_do_arquivo": rows,
-        })
-        instance.enqueue(process_dict)
-        print(process_dict)
+    if not is_file_unique(path_file, instance):
+        return
+    rows = txt_importer(path_file)
+    file_metadata = dict({
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(rows),
+        "linhas_do_arquivo": rows,
+    })
+    instance.enqueue(file_metadata)
+    print(file_metadata)
 
 
 def remove(instance):
